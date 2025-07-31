1dee15f43c9e46e505fa67ccf0cde141ed66ce35
Raymond <raymondurrutia2@yahoo.com>
2025-07-31 06:21:01 -0400

    Dynamically create menu from data

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
 import { menuArray } from "./data/data";
 
+document.addEventListener("click", (event) => {
+  if (event.target.dataset.addToCart) {
+    console.log(
+      "Add to cart clicked for item ID:",
+      event.target.dataset.addToCart
+    );
+  }
+});
+
 function getMenu() {
   let menuHtml = ``;
 
-  menuArray.map((item) => {
+  menuArray.forEach((item) => {
     menuHtml += `
       <div class="menu-item">
         <div class="menu-item_details">
           <span class="menu-item_emoji">${item.emoji}</span>
           <div class="menu-item_details_text">
             <h2 class="menu-item_details_name">${item.name}</h2>
-            <p class="menu-item_details_ingredients">${item.ingredients}</p>
+            <p class="menu-item_details_ingredients">${item.ingredients.join(
+              ", "
+            )}</p>
             <p class="menu-item_details_price">${item.price}</p>
           </div>
         </div>
-        <span class="menu-item_circle-btn">
+        <span class="menu-item_circle-btn" data-add-to-cart="${item.id}">
           <i class="fas fa-plus"></i>
         </span>
       </div>
